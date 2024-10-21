1525bfc6d35f13d57d13b92495aad6df612e16ce
jeffery017 <jeffery017@gmail.com>
2024-10-21 13:44:10 -0500

    change port

--- ipcam/main.py
+++ ipcam/main.py
@@ -71,4 +71,4 @@ def encode_image_to_base64(image_path: str) -> str:
 
 
 if __name__ == "__main__": 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
